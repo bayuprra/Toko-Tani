d7c4260d3334340233ee4c1dfde6fb8714ad1f56
Bayu Pratama <bayupratamaggg168@gmail.com>
2023-09-29 13:31:34 +0700

    first commit

---
+++ resources/views/layout/Admin_Layout/produk.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.Admin_layout.main_layout.main')
+
+@section('content')
+
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <p>produk</p>
+      <!-- /.row -->
+    </div><!--/. container-fluid -->
+  </section>
+
+@endSection
+
